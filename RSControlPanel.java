0ed549c6e3296503ec5e38356e109a653f1fb22d
automofearmotron <automofearmotron.noreply.github.com>
2014-07-26 23:08:52 -0400

    Minor bug fix

--- RSControlPanel.java
+++ RSControlPanel.java
@@ -17,9 +17,21 @@ public class RSControlPanel extends JFrame
 {
 	private int WIDTH;
 	private int HEIGHT;
+	private boolean hot;
+	private boolean fresh;
+	private boolean all;
+	private boolean html;
+	private boolean api;
 	private JButton start;
 	private JButton stop;
+   private JButton clearConsole;
+	private JRadioButton hotButton;
+	private JRadioButton freshButton;
+	private JRadioButton htmlButton;
+	private JRadioButton apiButton;
+	private JCheckBox allCheckBox;
 	private JTextField redditLocationField;
+	private JTextField subredditField;
 	private redditScraper rsScraper;
 	private Thread activeThread;
 	private RSEditorPane consolePane;
@@ -28,7 +40,7 @@ public class RSControlPanel extends JFrame
    private ArrayList<redditScraper> RSThreads = new ArrayList<redditScraper>();
 	
    private int numThreads = 0;
-	private String REDDIT_ROOT = "http://www.reddit.com/r/all/";
+	private String REDDIT_ROOT = "all";
 	
 	public RSControlPanel()
 	{
@@ -68,14 +80,22 @@ public class RSControlPanel extends JFrame
 		c.anchor = GridBagConstraints.FIRST_LINE_END;
 		c.gridx = 0;
 		c.gridy = 0;
-		c.weightx = 0.8;
+		c.weightx = 0.75;
 		
-		redditLocationField = new JTextField(100);
+		
+		redditLocationField = new JTextField(50);
 		redditLocationField.setText(REDDIT_ROOT);
 		add(redditLocationField,c);
 		
 		c.gridx = 1;
-		c.weightx = .1;
+		c.weightx = .75;
+		
+		subredditField = new JTextField(50);
+		subredditField.setText("Set Subreddit Destination");
+		add(subredditField,c);
+		
+		c.weightx = .10;
+		c.gridx = 2;
 		
 		start = new JButton("Start");
 		start.addActionListener(new ActionListener(){
@@ -86,7 +106,7 @@ public class RSControlPanel extends JFrame
 		});
 		add(start,c);
 		
-		c.gridx = 2;
+		c.gridx = 3;
 		
 		stop = new JButton("Stop");
 		stop.addActionListener(new ActionListener(){
@@ -96,14 +116,68 @@ public class RSControlPanel extends JFrame
 			}
 		});
 		add(stop,c);
+      
+      c.gridx=4;
+      
+      clearConsole = new JButton("Clear Console");
+		clearConsole.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				consolePane.setBody("");
+			}
+		});
+      
+		add(clearConsole,c);
 		//---------------------------------------------
+		
+		c.gridx= 0;
+		c.gridy = 1;
+		c.weightx = .75;
+		c.weighty = 0;
+		c.gridwidth = 1;
+		c.fill = GridBagConstraints.BOTH;
+		c.anchor = GridBagConstraints.LINE_START;
+		
+		hotButton = new JRadioButton("HOT");
+		hotButton.setSelected(true);
+		
+		freshButton = new JRadioButton("NEW");
+		htmlButton = new JRadioButton("HTML");
+		apiButton = new JRadioButton("API");
+		allCheckBox = new JCheckBox("r/all");
+		allCheckBox.setSelected(false);
+		
+		hotButton.addItemListener(new guiButtonListener());
+		freshButton.addItemListener(new guiButtonListener());
+		htmlButton.addItemListener(new guiButtonListener());
+		apiButton.addItemListener(new guiButtonListener());
+		allCheckBox.addItemListener(new guiButtonListener());
+		allCheckBox.setSelected(true);
+		
+		ButtonGroup popularityType = new ButtonGroup();
+		popularityType.add(hotButton);
+		popularityType.add(freshButton);
+		add(hotButton,c);
+		c.gridx=1;
+		add(freshButton,c);
+		c.gridx=2;
+		ButtonGroup requestType = new ButtonGroup();
+		requestType.add(htmlButton);
+		requestType.add(apiButton);
+		add(htmlButton,c);
+		c.gridx=3;
+		add(apiButton,c);
+		c.gridx=4;
+		add(allCheckBox,c);
+		
+		
 		
 		//---------------------------------------------
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 2;
 		c.weightx = 1;
 		c.weighty = 1;
-		c.gridwidth = 3;
+		c.gridwidth = 5;
 		c.fill = GridBagConstraints.BOTH;
 		
 		JPanel leftPane = new JPanel();
@@ -175,13 +249,17 @@ public class RSControlPanel extends JFrame
 	
 	public void startAThread()
 	{
-      if(numThreads < 3)
+      if(numThreads <= 4)
       {
    		scraper = new redditScraper();
          RSThreads.add(scraper);
    		scraper.setRedditLocation(redditLocationField.getText());
+			scraper.setSubmitSubreddit(subredditField.getText());
    		scraper.setConsoleRSPane(consolePane);
    		scraper.setDeleteRSPane(deletePane);
+			scraper.setUseAPIRequests(api);
+			scraper.setSearchTopPosts(hot);
+			scraper.setIsSubredditAll(all);
    		activeThread = new Thread(scraper);
    		activeThread.start();
          numThreads++;
@@ -204,5 +282,45 @@ public class RSControlPanel extends JFrame
 	public static void main(String[] args)
 	{
 		RSControlPanel visuals = new RSControlPanel();
+	}
+	
+	public class guiButtonListener implements ItemListener
+	{
+		public void itemStateChanged(ItemEvent e)
+		{
+			Object source = e.getItemSelectable();
+			
+			if(source == api)
+			{
+				api = true;
+				html = false;
+			}else if(source == html){
+				html = true;
+				api = false;
+			}else if(source == freshButton){
+				fresh = true;
+				hot = false;
+				api = true;
+				html = false;
+				allCheckBox.setSelected(false);
+				apiButton.setSelected(true);
+			}else if(source == hotButton){
+				hot = true;
+				fresh = false;
+			}else if(source == allCheckBox)
+			{
+				if(e.getStateChange() == ItemEvent.SELECTED) {
+					all = true;
+					html = true;
+					api = false;
+					fresh = false;
+					htmlButton.setSelected(true);
+        		}else{
+					all = false;
+					
+				}
+				
+			}
+		}
 	}
 }
